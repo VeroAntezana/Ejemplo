4a0a756bea25d8b90dbb96fb5ab8eeacdf8a2f89
VeroAntezana <ruth.veronica.9906@gmail.com>
2022-04-01 19:24:25 -0400

    Tarea Hola mundo

---
+++ Game.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using System;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Ejemplo
+{
+    public class Game: GameWindow
+    {
+        public Game()
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default) { 
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {   if(KeyboardState.IsKeyDown(Keys.Escape))
+                Close();
+            base.OnUpdateFrame(args);
+        } // Para que al pulsar escp salga del programa
+        //Aqui  se actualiza el codigo
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+        }// Renderiza el codigo como que en realidad aqui se dibuja todo 
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+
+namespace Ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game()) {
+                game.Run();
+            }
+        }
+
+    }
+}
