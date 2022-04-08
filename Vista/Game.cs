ca98cd319a390ba0b9e9873790fad96827651b24
VeroAntezana <ruth.veronica.9906@gmail.com>
2022-04-08 11:51:48 -0400

    triangulo

---
+++ Vista/Game.cs
@@ -0,0 +1,90 @@
+﻿using OpenTK;
+using System;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+
+namespace Grafica
+{
+    public class Game : GameWindow
+    {
+      //  private Escenario escenario = new Escenario();
+      //  private cubo cubito = new cubo(new Vector3(0, 0, 0), 2, 2, 2);
+        private int vertexBufferHandle;
+        public Game(GameWindowSettings config, NativeWindowSettings nativo) : base(config, nativo) { }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, Size.X, Size.Y);
+            base.OnResize(e);
+        }
+        float[] vertices = new float[] {
+                0.0f,0.5f,0f,
+                0.5f,-0.5f,0f,
+                -0.5f,-0.5f,0f,
+            };
+        int vao;
+        int vbo;
+        protected override void OnLoad()
+        {
+             GL.ClearColor(new Color4(0.3f, 0.3f, 0.2f, 0.2f)); // esto decide el color  
+             vao = GL.GenVertexArray();
+             GL.BindVertexArray(vao);
+             GL.GenBuffer();
+             vbo= GL.GenBuffer();
+             GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+             GL.BufferData(
+                 BufferTarget.ArrayBuffer,
+                 vertices.Length * sizeof(float),
+                 vertices.ToArray(),
+                 BufferUsageHint.StaticDraw);
+             GL.EnableVertexAttribArray(0);
+             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, true, 0, 0);
+
+
+
+            /* this.vertexBufferHandle = GL.GenBuffer();
+             GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferHandle);
+             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);*/
+
+
+           // escenario.addDibujo("cubo", cubito);
+            base.OnLoad();
+        }
+        protected override void OnUnload()
+        {
+
+            base.OnUnload();
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+                Close();
+            base.OnUpdateFrame(args);
+        } // Para que al pulsar escp salga del programa
+        //Aqui  se actualiza el codigo
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            
+           GL.BindVertexArray(vao);
+            GL.DrawArrays(BeginMode.Triangles, 0, vertices.Length);
+           // this.Context.SwapBuffers();
+          /*  base.OnRenderFrame(args);
+        }// Renderiza el codigo como que en realidad aqui se dibuja todo */
+          //  GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            //GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            
+            Context.SwapBuffers();
+            base.OnRenderFrame(args);
+
+        }
+
+
+    }
+}
---
+++ Vista/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+
+namespace Grafica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindowSettings config = new GameWindowSettings();
+            config.RenderFrequency = 30.0;
+            config.UpdateFrequency = 60.0;
+            NativeWindowSettings nativo = new NativeWindowSettings();
+            nativo.Size = new Vector2i(800, 600);
+            nativo.Title = "Programacion Grafica";
+            using (Game game = new Game(config, nativo))
+            {
+                game.Run();
+            }
+        }
+
+    }
+
+}
+
