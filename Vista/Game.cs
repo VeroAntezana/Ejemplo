d503a2a397e78978e24095ec7c47951be58f72c2
VeroAntezana <ruth.veronica.9906@gmail.com>
2022-04-09 08:15:02 -0400

    Basica casa

--- Vista/Game.cs
+++ Vista/Game.cs
@@ -10,9 +10,10 @@ namespace Grafica
 {
     public class Game : GameWindow
     {
-      //  private Escenario escenario = new Escenario();
-      //  private cubo cubito = new cubo(new Vector3(0, 0, 0), 2, 2, 2);
+        //  private Escenario escenario = new Escenario();
+        //  private cubo cubito = new cubo(new Vector3(0, 0, 0), 2, 2, 2);
         private int vertexBufferHandle;
+        private int shaderProgramHandle;
         public Game(GameWindowSettings config, NativeWindowSettings nativo) : base(config, nativo) { }
 
         protected override void OnResize(ResizeEventArgs e)
@@ -21,9 +22,9 @@ namespace Grafica
             base.OnResize(e);
         }
         float[] vertices = new float[] {
-            
+
                 0.0f,0.3f,0f,
-                0.3f,-0.3f,0f,
+                0.3f,-0.3f,0f,//2
                 -0.3f,-0.3f,0f, //3
               
               0.0f,0.3f,0f, //1 
@@ -36,21 +37,31 @@ namespace Grafica
 
             -0.3f,-0.3f,0f,//1
              -0.3f,-0.8f,0f,//2 //triangulo 4
-             0.3f,-0.3f,0f,
+             0.3f,-0.3f,0f,//3
 
+             0.3f,-0.3f,0f,//1
+             -0.3f,-0.8f,0f,//2 //triangulo5
+             0.3f,-0.8f,0f, //3
 
+             0.3f,-0.3f,0f,//1
+             0.3f,-0.8f,0f,//2 //triangulo 6
+             0.8f,-0.8f,0f,//3
 
-
+             0.3f,-0.3f,0f,//1
+             0.8f,-0.3f,0f,//2  triangulo 7
+             0.8f,-0.8f,0f//3
 
 
             };
+
         int vao;
         int vbo;
         
         protected override void OnLoad()
         {
              GL.ClearColor(new Color4(0.3f, 0.3f, 0.2f, 0.2f)); // esto decide el color  
-             vao = GL.GenVertexArray();
+            Color4 coral = new Color4(1.0f, 0.5f, 0.31f, 1.0f);
+            vao = GL.GenVertexArray();
              GL.BindVertexArray(vao);
              GL.GenBuffer();
              vbo= GL.GenBuffer();
@@ -65,10 +76,10 @@ namespace Grafica
 
 
 
-            /* this.vertexBufferHandle = GL.GenBuffer();
+             this.vertexBufferHandle = GL.GenBuffer();
              GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferHandle);
              GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);*/
+             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 
 
 
@@ -92,10 +103,39 @@ namespace Grafica
                     pixeColor= vec4(0.8,0.8f,0.1f,1f);
                 }
                 ";
+
+            int vertexShaderHandle = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShaderHandle, vertexShaderCode);
+            GL.CompileShader(vertexShaderHandle);
+
+            int pixelShaderHandle = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(pixelShaderHandle, pixelShaderCode);
+            GL.CompileShader (pixelShaderHandle);
+
+            this.shaderProgramHandle = GL.CreateProgram();
+            GL.AttachShader(this.shaderProgramHandle, vertexShaderHandle);
+            GL.AttachShader(this.shaderProgramHandle, pixelShaderHandle);
+            GL.LinkProgram(this.shaderProgramHandle);
+
+            GL.DetachShader(this.shaderProgramHandle, vertexShaderHandle);
+            GL.DetachShader (this.shaderProgramHandle, pixelShaderHandle);
+
+            GL.DeleteShader(vertexShaderHandle);
+            GL.DeleteShader (pixelShaderHandle);
+
+
+
             base.OnLoad();
         }
         protected override void OnUnload()
         {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(this.vertexBufferHandle);
+
+            GL.UseProgram(0);
+            GL.DeleteProgram(this.shaderProgramHandle);
+
+
 
             base.OnUnload();
         }
@@ -111,8 +151,9 @@ namespace Grafica
         {
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            
+            GL.UseProgram(this.shaderProgramHandle);
            GL.BindVertexArray(vao);
+            //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
             GL.DrawArrays(BeginMode.Triangles, 0, vertices.Length);
            // this.Context.SwapBuffers();
           /*  base.OnRenderFrame(args);
